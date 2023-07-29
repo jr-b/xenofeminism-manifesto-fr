c5e7883689a08e047b0bff854973b199b343eac9
jr-b <j@jrb.nz>
2023-07-29 11:10:32 -0400

    pdf print from pdf nup

--- script.sh
+++ script.sh
@@ -18,7 +18,7 @@ echo "running pdfjam - normal pdf"
 ./pdfjam-3.06/bin/pdfjam --batch --nup 2x1 --suffix 2up --twoside --openright true --no-landscape --outfile output/ output/out.pdf
 
 echo "running pdfjam - print pdf"
-./pdfjam-3.06/bin/pdfjam --suffix 2up-print --twoside --openright true --signature 8 --outfile output/ output/out.pdf 
+./pdfjam-3.06/bin/pdfjam --suffix 2up-print --twoside --openright true --signature 8 --outfile output/ output/out-2up.pdf 
 
 echo "deleting stuff"
 rm pdfjam-3.06.tar.gz
