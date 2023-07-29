9ac457f481666d3926ad37f1a7ad2d007feb5ceb
jr-b <j@jrb.nz>
2023-07-28 21:31:15 -0400

    test script post pandoc

--- script.sh
+++ script.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
+# This script will generate a PDF from LaTeX and Markdown files
+# We're using the pandoc tool to generate the PDF https://pandoc.org/
+
 pandoc -o output/out.pdf --pdf-engine=xelatex --template=input/main.tex input/manifesto.md
+
+sleep 10
+
+touch output/test.txt
